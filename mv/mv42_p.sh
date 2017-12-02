d70b1208447ce7cbcbc3239809aa27e8be51301d
Aneet D Narendranath <dnaneet@mtu.edu>
2017-12-01 20:46:20 -0500

    Add files via upload
    Parallelized mv42.sh

---
+++ mv/mv42_p.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+clear
+
+if [ -e mdl_file_summary.txt ]
+ then
+    rm mdl_file_summary.txt
+fi
+
+#Replace spaces in file names with underscores
+for f in *\ *; do mv "$f" "${f// /_}"; done
+
+echo "Creating a summary file with input data from all .mdl files in current folder"
+echo ""
+echo ""
+
+#Function that does all the processing with the .mdl files
+funcProcessing () {   
+        #echo $FILENAME
+        #Count the number of joints
+        nTrans=$(grep -o 'TransJoint' $FILENAME | wc -l)
+        nRev=$(grep -o 'RevJoint' $FILENAME | wc -l)
+        nFixed=$(grep -o 'FixedJoint' $FILENAME | wc -l)
+        nBall=$(grep -o 'BallJoint' $FILENAME | wc -l)
+        
+        nMotion=$(grep -o 'SetMotion' $FILENAME | wc -l)
+        motionOnJoint=$(grep "*Motion" $FILENAME | awk '{print $6}')
+        motionType=$(grep "SetMotion" $FILENAME | awk '{print $2}')
+        motionType=$(grep "SetMotion" $FILENAME | awk '{print $3}')
+        motionExp=$(grep "SetMotion" $FILENAME | awk '{print $5}')
+        #echo $nTrans
+        #Translational joint
+        connTrans=($(grep 'TransJoint' $FILENAME))
+        connRev=($(grep 'RevJoint' $FILENAME))    
+        connFixed=($(grep 'FixedJoint' $FILENAME))    
+        connBall=($(grep 'BallJoint' $FILENAME))    
+        #jt=$(grep 'TransJoint' $FILENAME | awk '{print $1}')
+        #jid=$(grep 'TransJoint' $FILENAME | awk '{print $2}')
+        #jname=$(grep 'TransJoint' $FILENAME | awk '{print $3}')
+        #b1=$(grep 'TransJoint' $FILENAME | awk '{print $4}')
+        #b2=$(grep 'TransJoint' $FILENAME | awk '{print $5}')  
+        #p1=$(grep 'TransJoint' $FILENAME | awk '{print $6}')    
+        #direction=$(grep 'TransJoint' $FILENAME | awk '{print $8}')    
+        
+        printf '%s\n ' "------------------------------------" >> mdl_file_summary.txt
+        printf '%s\n ' $FILENAME "--------------------------------" >> mdl_file_summary.txt
+        printf '%s ' "Number of Translational Joints:" $nTrans "," "Number of Revolute Joints: " $nRev "," "Number of Fixed Joints: " $nFixed "," "Number of Ball Joints:" $nBall "," "Number of Motions:" $nMotion "," "Motion applied to:" $motionOnJoint "Motion type: "$motionType "," "Expression:"$motionExp >> mdl_file_summary.txt   
+        echo $'\n' >> mdl_file_summary.txt
+        printf '%s\n' "Translational Joint" ${connTrans[@]} " " " " >> mdl_file_summary.txt
+        echo $'\n' >> mdl_file_summary.txt
+        printf '%s\n'  "Revolute Joint" ${connRev[@]}  " " " " >> mdl_file_summary.txt
+        echo $'\n' >> mdl_file_summary.txt
+        printf '%s\n' "Fixed Joint" ${connFixed[@]} " " " " >> mdl_file_summary.txt
+        echo $'\n' >> mdl_file_summary.txt     
+        printf '%s\n' "Ball Joint" ${connBall[@]} " " " " >> mdl_file_summary.txt
+        echo $'\n' >> mdl_file_summary.txt
+        echo $'**********************************************************************\n' >> mdl_file_summary.txt                 
+}
+
+
+for FILENAME in $( ls *.mdl); do
+    funcProcessing &
+done
+wait 
+
+
+
+  
+ #$jid $jname $b1 $b2 $propShell $p1 $direction
